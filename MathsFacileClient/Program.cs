1161c14f6e6aeba7a29090dcb43e11c994c0b61e
ismailnguyen <me@nguyenismail.com>
2016-11-07 12:30:32 +0100

    Add simple rest client for MathsFacile Api

---
+++ MathsFacileClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using System;
+
+namespace MathsFacileClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://mathsfacileapi20161107121521.azurewebsites.net");
+
+            var request = new RestRequest("calcul/", Method.POST);
+
+            request.AddJsonBody("4+4*4/12");
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+
+            Console.WriteLine(response.Content);
+
+            Console.Read();
+        }
+    }
+}
