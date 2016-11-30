07a88fd601433184517d62d81a5ff273da2e6d2c
ismailnguyen <me@nguyenismail.com>
2016-11-30 22:26:40 +0100

    Test should pass

--- MathsFacileClient.Tests/CalculTetsts.cs
+++ MathsFacileClient.Tests/CalculTetsts.cs
@@ -55,15 +55,15 @@ namespace MathsFacileClient.Tests
         }
 
         [TestMethod]
-        public void Should_fail()
+        public void Should_not_fail()
         {
             var homeController = new HomeController();
 
-            var calcul = "2/5";
+            var calcul = "5+5";
 
             var result = homeController.Calcul(calcul);
 
-            Assert.AreEqual(result, "10"); //doit échouer !
+            Assert.AreEqual(result, "10");
         }
     }
 }
